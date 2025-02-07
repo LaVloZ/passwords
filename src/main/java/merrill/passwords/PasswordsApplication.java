41ea3fdb78c5f6299abf5007ae4ddc7ed4aaddd9
Abdessamed MANSOURI <lavlozm@gmail.com>
2025-02-07 01:35:27 +0100

    project init

---
+++ src/main/java/merrill/passwords/PasswordsApplication.java
@@ -0,0 +1,13 @@
+package merrill.passwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordsApplication.class, args);
+	}
+
+}
